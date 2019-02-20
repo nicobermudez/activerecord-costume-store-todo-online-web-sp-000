1d914744e750d05a16cdfcda4b910080d97c5943
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-20 16:55:55 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,7 +3,7 @@ class CostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
       t.string :name
-      t.float :price
+      t.string :location
       t.string :image_url
       t.string :size
     end
