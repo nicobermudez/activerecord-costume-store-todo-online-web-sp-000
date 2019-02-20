4a9c9c163dbd697464b5055f3f8c10ddda240835
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-20 16:48:51 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -2,6 +2,10 @@
 class CostumeStore << ActiveRecord::Migration[5.1]
   def change
     create_table :costume do |t|
+      t.string :name
+      t.string :price
+      t.string :image_url
+      t.string :size
     end
   end
 end
