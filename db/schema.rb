a90f13564135af581f23d1fa23ed9e524c5546ec
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-20 17:02:51 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.boolean "family_friendly"
     t.datetime "opening_date"
     t.datetime "closing_date"
-    t.string "desription"
+    t.string "description"
   end
 
 end
